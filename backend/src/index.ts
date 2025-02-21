04218ac91155c228f8957ae9636a4d9cc0954ba3
Zachary Burns <114180449+pikkauhn@users.noreply.github.com>
2025-02-21 08:58:14 -0600

    New backend repo from nooroTask Repo

---
+++ backend/src/index.ts
@@ -0,0 +1,48 @@
+import express, { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+import cors from "cors";
+
+const app = express();
+const prisma = new PrismaClient();
+const port = 3001;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/tasks", async (req: Request, res: Response) => {
+  const tasks = await prisma.task.findMany();
+  res.json(tasks);
+});
+
+app.get("/tasks/:id", async (req: Request, res: Response) => {
+  const tasks = await prisma.task.findFirst();
+  res.json(tasks);
+});
+
+app.post("/tasks", async (req: Request, res: Response) => {
+  const task = await prisma.task.create({
+    data: req.body,
+  });
+  res.json(task);
+});
+
+app.put('/tasks/:id', async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const task = await prisma.task.update({
+        where: { id: parseInt(id) },
+        data: req.body,
+    });
+    res.json(task);
+});
+
+app.delete('/tasks/:id', async (req: Request, res: Response) => {
+    const { id } = req.params;
+    await prisma.task.delete({
+        where: { id: parseInt(id) },
+    });
+    res.sendStatus(204);
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
